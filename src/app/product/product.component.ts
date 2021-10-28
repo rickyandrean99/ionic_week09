04cc4be8535f22b10d2eea4a229964f3dbcaa5d0
rickyandrean99 <ricky.ralph09@gmail.com>
2021-10-28 15:35:53 +0700

    Create Phone List

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,6 +12,57 @@ export class ProductComponent implements OnInit {
     btnDisable: boolean = true
     btnColor = 'primary'
     netPrice: number = this.productPrice
+    fontSize: number = 12
+
+    products = [
+        { 
+            name: 'Realme 5',
+            price: 1800000,
+            disc: 0.1,
+            url: 'realme_5.jpg',
+            spec: ['4 GB RAM', 'Qualcomm SDM665 Snapdragon 665', '128 GB Storage'],
+            open: false
+        },
+        {
+            name: 'OPPO A1',
+            price: 1400000,
+            disc: 0.15,
+            url: 'oppo_a1.jpg',
+            spec: ['4 GB RAM','Mediatek MT6763T Helio P23','64 GB Storage'],
+            open: false
+        },
+        { 
+            name: 'Samsung Galaxy S10', 
+            price: 1200000,
+            disc: 0, 
+            url: 'samsung10.jpg',
+            spec: ['8 GB RAM','Exynos 9820','512 GB Storage'],
+            open: false
+        },
+        { 
+            name: 'Redmi Note 8', 
+            price: 1900000,
+            disc: 0.2, 
+            url: 'redmi_note_8.jpg',
+            spec: ['6 GB RAM','Qualcomm SDM665 Snapdragon 665','128 GB Storage'],
+            open: false
+        },
+        { 
+            name: 'Iphone X', 
+            price: 6300000, 
+            disc: 0.25, 
+            url: 'iphone_x.jpg',
+            spec: ['3 GB RAM','Apple A11 Bionic','256 GB Storage'],
+            open: false
+        }
+    ]
+
+    showClick(index: number) {
+        if (this.products[index].open)
+            this.products[index].open = false
+        else
+            this.products[index].open = true
+    }
 
     checkVoucher(event: Event) {
         var element = event.target as HTMLInputElement
@@ -30,5 +81,4 @@ export class ProductComponent implements OnInit {
     constructor() { }
 
     ngOnInit() { }
-
 }
